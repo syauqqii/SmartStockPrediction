65e939e159683f48307261aa033b7f23695c0bb7
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-15 06:32:02 +0700

    Update LOG

--- Controllers/Admin/KategoriProdukController.go
+++ Controllers/Admin/KategoriProdukController.go
@@ -15,6 +15,7 @@ func ListKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.Find(&kategoriProduks).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusInternalServerError, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> ListKategoriProduk() - 1")
 		return
 	}
 
@@ -29,6 +30,7 @@ func ListKategoriProduk(w http.ResponseWriter, r *http.Request) {
 
 	response := Models.KategoriProdukListResponse{KategoriProduks: kategoriProdukResponses}
 	Utils.ResponseJSON(w, http.StatusOK, response)
+	Utils.Logger(3, "Admin/kategoriProduk.go -> ListKategoriProduk()")
 }
 
 func CreateKategoriProduk(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +39,7 @@ func CreateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Utils.DecodeJSONBody(w, r, &kategoriProdukInput); err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusBadRequest, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> CreateKategoriProduk() - 1")
 		return
 	}
 
@@ -45,6 +48,7 @@ func CreateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.Where("nama_kategori_produk = ?", kategoriProdukInput.NamaKategoriProduk).First(&existingKategoriProduk).Error; err == nil {
 		response := map[string]string{"message": "nama kategori sudah ada"}
 		Utils.ResponseJSON(w, http.StatusConflict, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> CreateKategoriProduk() - 2")
 		return
 	}
 
@@ -55,11 +59,13 @@ func CreateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.Create(&kategoriProduk).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusInternalServerError, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> CreateKategoriProduk() - 3")
 		return
 	}
 
 	response := map[string]string{"message": "berhasil menambahkan kategori produk"}
 	Utils.ResponseJSON(w, http.StatusCreated, response)
+	Utils.Logger(3, "Admin/kategoriProduk.go -> CreateKategoriProduk()")
 }
 
 func GetKategoriProdukByID(w http.ResponseWriter, r *http.Request) {
@@ -68,6 +74,7 @@ func GetKategoriProdukByID(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusBadRequest, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> GetKategoriProdukByID() - 1")
 		return
 	}
 
@@ -76,6 +83,7 @@ func GetKategoriProdukByID(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.First(&kategoriProduk, kategoriProdukID).Error; err != nil {
 		response := map[string]string{"message": "kategori produk tidak ditemukan"}
 		Utils.ResponseJSON(w, http.StatusNotFound, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> GetKategoriProdukByID() - 2")
 		return
 	}
 
@@ -84,6 +92,7 @@ func GetKategoriProdukByID(w http.ResponseWriter, r *http.Request) {
 		NamaKategoriProduk: kategoriProduk.NamaKategoriProduk,
 	}
 	Utils.ResponseJSON(w, http.StatusOK, response)
+	Utils.Logger(3, "Admin/kategoriProduk.go -> GetKategoriProdukByID()")
 }
 
 func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
@@ -92,6 +101,7 @@ func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusBadRequest, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> UpdateKategoriProduk() - 1")
 		return
 	}
 
@@ -99,6 +109,7 @@ func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.First(&kategoriProduk, kategoriProdukID).Error; err != nil {
 		response := map[string]string{"message": "kategori produk tidak ditemukan"}
 		Utils.ResponseJSON(w, http.StatusNotFound, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> UpdateKategoriProduk() - 2")
 		return
 	}
 
@@ -106,6 +117,7 @@ func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Utils.DecodeJSONBody(w, r, &kategoriProdukInput); err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusBadRequest, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> UpdateKategoriProduk() - 3")
 		return
 	}
 
@@ -113,6 +125,7 @@ func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.Where("nama_kategori_produk = ? AND id_kategori_produk != ?", kategoriProdukInput.NamaKategoriProduk, kategoriProdukID).First(&existingKategoriProduk).Error; err == nil {
 		response := map[string]string{"message": "nama kategori sudah ada"}
 		Utils.ResponseJSON(w, http.StatusConflict, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> UpdateKategoriProduk() - 4")
 		return
 	}
 
@@ -121,11 +134,13 @@ func UpdateKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.Save(&kategoriProduk).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusInternalServerError, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> UpdateKategoriProduk() - 5")
 		return
 	}
 
 	response := map[string]string{"message": "berhasil update kategori produk"}
 	Utils.ResponseJSON(w, http.StatusOK, response)
+	Utils.Logger(3, "Admin/kategoriProduk.go -> UpdateKategoriProduk()")
 }
 
 func DeleteKategoriProduk(w http.ResponseWriter, r *http.Request) {
@@ -134,6 +149,7 @@ func DeleteKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusBadRequest, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> DeleteKategoriProduk() - 1")
 		return
 	}
 
@@ -141,15 +157,18 @@ func DeleteKategoriProduk(w http.ResponseWriter, r *http.Request) {
 	if err := Database.DB.First(&kategoriProduk, kategoriProdukID).Error; err != nil {
 		response := map[string]string{"message": "kategori produk tidak ditemukan"}
 		Utils.ResponseJSON(w, http.StatusNotFound, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> DeleteKategoriProduk() - 2")
 		return
 	}
 
 	if err := Database.DB.Delete(&kategoriProduk).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		Utils.ResponseJSON(w, http.StatusInternalServerError, response)
+		Utils.Logger(2, "Admin/kategoriProduk.go -> DeleteKategoriProduk() - 3")
 		return
 	}
 
 	response := map[string]string{"message": "berhasil hapus kategori produk"}
 	Utils.ResponseJSON(w, http.StatusOK, response)
+	Utils.Logger(3, "Admin/kategoriProduk.go -> DeleteKategoriProduk()")
 }
