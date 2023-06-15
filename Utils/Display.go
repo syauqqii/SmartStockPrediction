8c43b1f269741e98c222003bdfea094f3c30e967
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-15 07:23:23 +0700

    Update

--- Utils/Display.go
+++ Utils/Display.go
@@ -3,30 +3,32 @@ package Utils
 import "fmt"
 
 func Display() {
-	fmt.Printf("\n   > Endpoint (Admin) :")
-	fmt.Printf("\n     - /users      -> GET         |  - /pelanggan      -> GET       |  - /kategori-produk      -> GET")
-	fmt.Printf("\n     - /users      -> POST        |  - /pelanggan      -> POST      |  - /kategori-produk      -> POST")
-	fmt.Printf("\n     - /users/{id} -> GET         |  - /pelanggan/{id} -> GET       |  - /kategori-produk/{id} -> GET")
-	fmt.Printf("\n     - /users/{id} -> PUT         |  - /pelanggan/{id} -> PUT       |  - /kategori-produk/{id} -> PUT")
-	fmt.Printf("\n     - /users/{id} -> DELETE      |  - /pelanggan/{id} -> DELETE    |  - /kategori-produk/{id} -> DELETE")
 
-	fmt.Printf("\n\n     - /produk      -> GET        |  - /keranjang      -> GET       |")
-	fmt.Printf("\n     - /produk      -> POST       |  - /keranjang      -> POST      |  - /transaksi      -> GET")
-	fmt.Printf("\n     - /produk/{id} -> GET        |  - /keranjang/{id} -> GET       |  - /transaksi      -> POST")
-	fmt.Printf("\n     - /produk/{id} -> PUT        |  - /keranjang/{id} -> PUT       |  - /transaksi/{id} -> GET")
-	fmt.Printf("\n     - /produk/{id} -> DELETE     |  - /keranjang/{id} -> DELETE    |  - /transaksi/{id} -> DELETE")
+	fmt.Printf("\n   > Request Methods :")
+	fmt.Printf("\n     - GET    : Read, Read {id}  | - PUT    : Update {id}")
+	fmt.Printf("\n     - POST   : Create           | - DELETE : Delete {id}")
 
-	fmt.Printf("\n\n    ==============================================================================================")
+	fmt.Printf("\n\n    ===================================================================================================")
 
-	fmt.Printf("\n\n   > Endpoint (Kasir) :")
-	fmt.Printf("\n     - /pelanggan      -> GET     |")
-	fmt.Printf("\n     - /pelanggan      -> POST    |")
-	fmt.Printf("\n     - /pelanggan/{id} -> GET     |  - /kategori-produk      -> GET |  - /produk      -> GET")
-	fmt.Printf("\n     - /pelanggan/{id} -> PUT     |  - /kategori-produk/{id} -> GET |  - /produk/{id} -> GET")
+	fmt.Printf("\n\n   > Endpoint Admin [Prefix: Admin] :")
+	fmt.Printf("\n     - /users                -> GET, POST         |  - /pelanggan      -> GET, POST       ")
+	fmt.Printf("\n     - /users/{id}           -> GET, PUT, DELETE  |  - /pelanggan/{id} -> GET, PUT, DELETE")
 
-	fmt.Printf("\n\n     - /keranjang      -> GET     |                                 |  - /detail-transaksi      -> GET")
-	fmt.Printf("\n     - /keranjang      -> POST    |  - /transaksi      -> GET       |  - /detail-transaksi      -> POST")
-	fmt.Printf("\n     - /keranjang/{id} -> GET     |  - /transaksi      -> POST      |  - /detail-transaksi/{id} -> GET")
-	fmt.Printf("\n     - /keranjang/{id} -> PUT     |  - /transaksi/{id} -> GET       |  - /detail-transaksi/{id} -> PUT")
-	fmt.Printf("\n     - /keranjang/{id} -> DELETE  |  - /transaksi/{id} -> DELETE    |  - /detail-transaksi/{id} -> DELETE")
+	fmt.Printf("\n\n     - /kategori-produk      -> GET, POST         |  - /produk         -> GET, POST")
+	fmt.Printf("\n     - /kategori-produk/{id} -> GET, PUT, DELETE  |  - /produk/{id}    -> GET, PUT, DELETE")
+
+	fmt.Printf("\n\n     - /rekomendasi-dari-stok      -> GET")
+	fmt.Printf("\n     - /rekomendasi-dari-penjualan -> GET")
+
+	fmt.Printf("\n\n    ===================================================================================================")
+
+	fmt.Printf("\n\n   > Endpoint Kasir [Prefix: Kasir] :")
+	fmt.Printf("\n     - /pelanggan      -> GET, POST         |  - /kategori-produk/      -> GET, POST       ")
+	fmt.Printf("\n     - /pelanggan/{id} -> GET, PUT, DELETE  |  - /kategori-produk/{id}  -> GET, PUT, DELETE")
+
+	fmt.Printf("\n\n     - /produk         -> GET, POST         |  - /keranjang/            -> GET, POST     ")
+	fmt.Printf("\n     - /produk/{id}    -> GET, PUT, DELETE  |  - /keranjang/{id}        -> GET, PUT, DELETE")
+
+	fmt.Printf("\n\n     - /transaksi      -> GET, POST         |  - /detail-transaksi      -> GET, POST")
+	fmt.Printf("\n     - /transaksi/{id} -> GET, PUT, DELETE  |  - /detail-transaksi/{id} -> GET, PUT, DELETE")
 }
