23631688b8e5e61869428f157693207cf1f20cd5
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-21 20:36:11 +0700

    Update LoadEnv.go

--- Utils/LoadEnv.go
+++ Utils/LoadEnv.go
@@ -37,7 +37,7 @@ func LoadEnv() {
 
 	// Lokal Variable
 	DB_USER := os.Getenv("DB_USER")
-	DB_PASS := os.Getenv("DB_PASSWORD")
+	DB_PASS := os.Getenv("DB_PASS")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
